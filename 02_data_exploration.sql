01a876a12cd5741d77e92145838a97bfbb9097f3
Hetvi48 <hetvikhadela48@gmail.com>
2026-02-21 20:59:44 +0530

    feature engineering

--- 02_data_exploration.sql
+++ 02_data_exploration.sql
@@ -3,3 +3,10 @@ use smart_watch_health;
 SELECT * FROM health_data;
 
 DESCRIBE health_data;
+
+select distinct(stress_level)
+from health_data
+order by stress_level;
+
+select min(heart_rate_BPM), max(heart_rate_BPM), min(blood_oxygen_level), max(blood_oxygen_level), min(step_count), max(step_count), min(sleep_duration_hr), max(sleep_duration_hr)
+from health_data;
---
+++ 03_feature_construction.sql
@@ -0,0 +1,56 @@
+use smart_watch_health;
+
+CREATE TABLE activity_intensity_table AS
+SELECT
+    user_id,
+    (step_count * heart_rate_BPM) AS activity_intensity
+FROM health_data;
+
+select * from activity_intensity_table;
+select min(activity_intensity), max(activity_intensity) 
+from activity_intensity_table;
+
+CREATE TABLE stress_sleep_ratio_table AS
+SELECT
+    user_id,
+    CASE
+        WHEN sleep_duration_hr > 0
+        THEN (stress_level / sleep_duration_hr)
+        ELSE NULL
+    END AS stress_sleep_ratio
+FROM health_data;
+
+select * from stress_sleep_ratio_table;
+select min(stress_sleep_ratio), max(stress_sleep_ratio) 
+from stress_sleep_ratio_table;
+
+CREATE TABLE health_score_table AS
+SELECT
+    user_id,
+    (
+        (blood_oxygen_level / 100) * 0.4 +
+        (sleep_duration_hr / 8) * 0.3 +
+        (1 - stress_level / 10) * 0.3
+    ) AS health_score
+FROM health_data;
+
+select * from health_score_table;
+select min(health_score), max(health_score)
+from health_score_table;
+
+SELECT 
+    h.*,
+    a.activity_intensity,
+    s.stress_sleep_ratio,
+    hs.health_score
+
+FROM health_data h
+
+LEFT JOIN activity_intensity_table a
+    ON h.user_id = a.user_id
+
+LEFT JOIN stress_sleep_ratio_table s
+    ON h.user_id = s.user_id
+
+LEFT JOIN health_score_table hs
+    ON h.user_id = hs.user_id;
